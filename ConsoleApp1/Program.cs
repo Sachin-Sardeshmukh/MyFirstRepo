9ae1acf743a320134dab953d126647d8730a495f
Sachin-Sardeshmukh <63308344+Sachin-Sardeshmukh@users.noreply.github.com>
2020-04-11 16:28:37 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //Master2
+            //Master4
         }
          
        }
